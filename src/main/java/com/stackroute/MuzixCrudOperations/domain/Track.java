dbed2ed46e9a360e16d7e420ad3bf21e0bd25926
Sai Poojitha <pujita.pabbathi@gmail.com>
2019-07-25 10:38:08 +0530

    lombok task

--- src/main/java/com/stackroute/MuzixCrudOperations/domain/Track.java
+++ src/main/java/com/stackroute/MuzixCrudOperations/domain/Track.java
@@ -1,5 +1,11 @@
 package com.stackroute.MuzixCrudOperations.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.Entity;
@@ -7,52 +13,56 @@ import javax.persistence.Id;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track
 {
     @Id
     private int id;
     private String name;
     private String comment;
-    public Track()
-    {}
-
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
+//    public Track()
+//    {}
+//
+//    public Track(int id, String name, String comment) {
+//        this.id = id;
+//        this.name = name;
+//        this.comment = comment;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getComment() {
+//        return comment;
+//    }
+//
+//    public void setComment(String comment) {
+//        this.comment = comment;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Track{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", comment='" + comment + '\'' +
+//                '}';
+//    }
 
 }
