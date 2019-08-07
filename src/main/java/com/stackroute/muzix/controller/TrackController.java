ae79fdad6c25f9c8d6c0e263c32611f7cd25b883
poojitha1997 <40695302+poojitha1997@users.noreply.github.com>
2019-08-07 16:28:08 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/muzix/controller/TrackController.java
@@ -16,7 +16,8 @@ import java.util.List;
 @RequestMapping(value = "api/v1")
 public class TrackController {
 
-    private TrackService trackService;
+private TrackService trackService;
+    ResponseEntity responseEntity;
     @Autowired
     public TrackController(TrackService trackService)
     {
@@ -26,7 +27,6 @@ public class TrackController {
     //saving the tracks
     @PostMapping("Tracks")
     public ResponseEntity<?> saveTracks(@RequestBody List<Track> tracks) throws TrackAlreadyExistsException{
-        ResponseEntity responseEntity;
         for(Track t1:tracks)
         {
             trackService.saveTrack(t1);
@@ -40,13 +40,16 @@ public class TrackController {
     
     @PostMapping("track")
     public ResponseEntity<?> saveTrack(@RequestBody Track track) {
-        ResponseEntity responseEntity;
         try {
             trackService.saveTrack(track);
             responseEntity = new ResponseEntity<String>("Successfully created", HttpStatus.CREATED);
 
         } catch (TrackAlreadyExistsException ex) {
             responseEntity = new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
+        }
+         catch(Exception e)
+        {
+            e.printStackTrace();
         }
 
         return responseEntity;
@@ -61,23 +64,24 @@ public class TrackController {
     @GetMapping("track/{id}")
     public ResponseEntity<?> getTrackById(@PathVariable(value = "id") Integer id)
     {
-        ResponseEntity responseEntity;
-
         try {
-
             responseEntity=  new ResponseEntity<Track>(trackService.getTrackById(id),HttpStatus.OK);
         }
         catch(TrackNotFoundException ex)
         {
             responseEntity=new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
         }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
         return responseEntity;
     }
     //deleting the track by id
     @DeleteMapping("track/{id}")
     public ResponseEntity<?> deleteTrackById(@PathVariable(value="id") Integer id)
     {
-        ResponseEntity responseEntity;
         trackService.deleteTrack(id);
         responseEntity=new ResponseEntity<String>("Deleted",HttpStatus.NO_CONTENT);
         return responseEntity;
@@ -85,7 +89,6 @@ public class TrackController {
     //updating the user 
     @PutMapping("track")
     public ResponseEntity<?> updateTrack(@RequestBody Track track)  {
-        ResponseEntity responseEntity;
         try {
 
             trackService.saveTrack(track);
@@ -95,6 +98,10 @@ public class TrackController {
 
             responseEntity = new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
         }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
             return responseEntity;
         }
@@ -103,7 +110,6 @@ public class TrackController {
    //@Query("from Track where name=?1")
     public ResponseEntity<?> getAllTracksByName(@PathVariable(value="name") String name)
     {
-        ResponseEntity responseEntity;
         try {
             responseEntity=new ResponseEntity<List<Track>>(trackService.getTrackByName(name),HttpStatus.OK);
         }
@@ -111,6 +117,11 @@ public class TrackController {
         {
             responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
         }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
        return responseEntity;
     }
 }
