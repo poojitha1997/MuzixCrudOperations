548e6fabcf3a9812c8f9d080175082fd314422b8
poojitha1997 <40695302+poojitha1997@users.noreply.github.com>
2019-08-07 12:00:32 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/MuzixCrudOperations/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/MuzixCrudOperations/service/TrackServiceImpl.java
@@ -33,8 +33,9 @@ public class TrackServiceImpl implements TrackService
     }
 
     @Override
-    public void deleteTrack(int id) {
+    public String deleteTrack(int id) {
         trackRepository.deleteById(id);
+        return "deleted";
 
     }
 
