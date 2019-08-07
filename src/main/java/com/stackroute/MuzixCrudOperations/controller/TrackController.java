349f4efe032d7aa57dd87a3982bca503c1baac45
poojitha1997 <40695302+poojitha1997@users.noreply.github.com>
2019-08-07 16:57:21 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/MuzixCrudOperations/controller/TrackController.java
+++ src/main/java/com/stackroute/MuzixCrudOperations/controller/TrackController.java
@@ -14,7 +14,7 @@ public class TrackController
 {
     private TrackService trackService;
 
-    ResponseEntity responseEntity;
+    private ResponseEntity responseEntity;
 
     @Autowired
     public TrackController(TrackService trackService) {
