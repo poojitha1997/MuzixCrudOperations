30c9e68392c1db8b36c17979e9d8baef02cfb5ff
poojitha1997 <40695302+poojitha1997@users.noreply.github.com>
2019-08-07 12:52:27 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/MuzixCrudOperations/controller/TrackController.java
+++ src/main/java/com/stackroute/MuzixCrudOperations/controller/TrackController.java
@@ -19,6 +19,7 @@ public class TrackController
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
     }
+        //saving the track
     @PostMapping("track")
     public ResponseEntity<?> saveTrack(@RequestBody Track track) {
         ResponseEntity responseEntity;
@@ -33,7 +34,7 @@ public class TrackController
 
         return responseEntity;
     }
-
+    //retrieving all the tracks
     @GetMapping("track")
     public ResponseEntity<?> getAllTracks()
     {
@@ -44,6 +45,7 @@ public class TrackController
     {
         return new ResponseEntity<Track>(trackService.getTrackById(id),HttpStatus.OK);
     }
+        //deleting the track by id
     @DeleteMapping("track/{id}")
     public ResponseEntity<?> deleteuserById(@PathVariable(value="id") Integer id)
     {
@@ -52,6 +54,7 @@ public class TrackController
         responseEntity=new ResponseEntity<String>("Deleted",HttpStatus.FORBIDDEN);
         return responseEntity;
     }
+        //updating the track
     @PutMapping("track")
     public ResponseEntity<?> updateUser(@RequestBody Track track)  {
         ResponseEntity responseEntity;
