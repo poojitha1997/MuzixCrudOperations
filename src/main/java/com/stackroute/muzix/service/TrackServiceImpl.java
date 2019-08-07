cc1018664103f28942c591dd00ce370f4723f761
poojitha1997 <40695302+poojitha1997@users.noreply.github.com>
2019-08-07 11:51:32 +0530

    made service methods return resource type

--- src/main/java/com/stackroute/muzix/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/muzix/service/TrackServiceImpl.java
@@ -40,8 +40,9 @@ public class TrackServiceImpl implements TrackService
     }
 
     @Override
-    public void deleteTrack(int id) {
+    public string deleteTrack(int id) {
         trackRepository.deleteById(id);
+        return "deleted";
     }
 
     @Override
